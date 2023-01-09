78fe05aee6dab64361b34e42277975334500afa5
Emil Wennerholm <121676728+EmilWennerholm@users.noreply.github.com>
2023-01-09 16:33:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Reading the data
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
 #Setting Fruits as Index
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
